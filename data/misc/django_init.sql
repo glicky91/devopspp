159c38aab1a1af6ae7afca1ec1ad013794cb75ca
“Hanna <hanna.glickman@gmail.com>
2023-01-20 15:02:03 -0800

    rest of file

---
+++ data/misc/django_init.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS DevOps_PP;
+CREATE DATABASE DevOps_PP;
+
+\c DevOps_PP
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
---
+++ mysite1/mysite/views.py
@@ -0,0 +1,56 @@
+# function file
+from django.shortcuts import render
+from django.http import HttpResponse
+from mysite.models import Post, Like
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+
+
+def index(request):
+    posts = Post.objects.all()
+
+    postLikeArray = []
+
+    # a for loop where we go through each of the items in the posts queryset and
+    # input the actual post object into the aray
+
+    for post in posts:
+        likes = Like.objects.filter(post=post)
+        mylist = [post, likes]
+        postLikeArray.append(mylist)
+
+    # Inside the first loop (per post), we look for all of the likes that are associated with that post and
+    # add to the second
+    template_variables = {
+        'foo': 'bar',
+        'username': 'hanna',
+        'postLikeArray': postLikeArray
+    }
+    return render(request, "index.html", context=template_variables)
+    # return HttpResponse("Hello, world!")
+
+
+@api_view(['GET', 'POST'])
+@csrf_exempt
+def masterendpoint(request):
+    # figure out the request type
+    # if type is create post, then create a new post and save to db
+    if request.data.get("type") == "newpost":
+        title = request.data.get("title")
+        author = User.objects.filter(
+            username=request.data.get("username")).first()
+        content = request.data.get("content")
+        newpost = Post.objects.create(
+            author=author, title=title, content=content)
+        newpost.save()
+        return HttpResponse()
+    template_variables = {
+        'foo': 'bar',
+        'username': 'hanna1',
+        'email': 'test1@test.com'
+    }
+
+    # return render(request, "index.html", context=template_variables)
+    return HttpResponse(User.objects.filter(
+        username=request.data.get("username")).first().email)
